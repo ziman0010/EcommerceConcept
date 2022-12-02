b9c13f8be19c819f47d3220d89c7401c0a6fec38
ziman <alex.cheranov@yandex.ru>
2022-12-02 11:52:48 +0300

    Initial Commit

---
+++ EffectiveMobileTest/EffectiveMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveMobileTestApp.swift
+//  EffectiveMobileTest
+//
+//  Created by Алексей Черанёв on 02.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
